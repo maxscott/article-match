43a2e16b95514b464650fb2d9ca48854a9021550
Max <max.weinbrown@gmail.com>
2014-05-09 13:12:33 -0400

    Looking pretty good

--- similizer.rb
+++ similizer.rb
@@ -1,24 +1,18 @@
 class Similizer
-  def cosine_similarity arr_of_name_vals1, arr_of_name_vals2
-    keywords1 = arr_of_name_vals1.clone
-    keywords2 = arr_of_name_vals2.clone
+  def cosine_similarity keywords1, keywords2
+    intersect_names = keywords1.map { |h| h[:name] } & keywords2.map { |h| h[:name] }
 
-    names1 = keywords1.map { |h| h[:name] }
-    names2 = keywords2.map { |h| h[:name] }
+    match_vals1 = keywords1
+      .select { |pair| intersect_names.any? { |name| name == pair[:name] } }
+      .sort! { |h1,h2| h1[:name] <=> h2[:name] }
+      .map { |h| h[:val] }
 
-    # for all words in A not in B, add them to B with val 0
-    # and the opposite
-    (names1 - names2).each { |word| keywords2 << {:name => word, :val => 0} }
-    (names2 - names1).each { |word| keywords1 << {:name => word, :val => 0} }
+    match_vals2 = keywords2
+      .select { |pair| intersect_names.any? { |name| name == pair[:name] } }
+      .sort! { |h1,h2| h1[:name] <=> h2[:name] }
+      .map { |h| h[:val] }
 
-    # intersect_names = keywords1.map { |h| h[:name] } & keywords2.map { |h| h[:name] }
-    #keywords1.filter { |pair| intersect_names.any? { |name| name == pair.name} }
-    #keywords2.filter { |pair| intersect_names.any? { |name| name == pair.name} }
-    
-    keywords1.sort! { |h1,h2| h1[:name] <=> h2[:name] }
-    keywords2.sort! { |h1,h2| h1[:name] <=> h2[:name] }
-
-    dot( keywords1.map { |h| h[:val] }, keywords2.map { |h| h[:val] } )
+    dot( match_vals1, match_vals2 )
   end
 
   :private
